a2b812f85e03cca1aca0a8f0256ab35aa1b7ea3a
SelfishMak <selfish.mak@gmail.com>
2021-10-11 21:17:30 +0300

    installed enum

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum ConditionLight {
+    case red, yelow, green
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var ligthRed: UIView!
     @IBOutlet weak var lightYelow: UIView!
@@ -14,38 +18,48 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var buttnLight: UIButton!
     
+    
     let lightOn: CGFloat = 1
     let lightOff: CGFloat = 0.3
+    var conditionLight = ConditionLight.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        buttnLight.layer.cornerRadius = 10
         
         ligthRed.alpha = lightOff
         lightYelow.alpha = lightOff
         lightGreen.alpha = lightOff
         
-        ligthRed.layer.cornerRadius = 50
-        lightYelow.layer.cornerRadius = 50
-        lightGreen.layer.cornerRadius = 50
+        buttnLight.layer.cornerRadius = 10
+
+        ligthRed.layer.cornerRadius = ligthRed.frame.height / 2
+        lightYelow.layer.cornerRadius = lightYelow.frame.height / 2
+        lightGreen.layer.cornerRadius = lightGreen.frame.height / 2
     }
+    
     
     @IBAction func buttnLightPressed() {
         if buttnLight.currentTitle == "START" {
             buttnLight.setTitle("NEXT", for: .normal)
+    }
+        
+        switch conditionLight {
+        case.red:
             ligthRed.alpha = lightOn
-            lightYelow.alpha = lightOff
             lightGreen.alpha = lightOff
-        } else if ligthRed.alpha == lightOn {
+            conditionLight = .yelow
+            
+        case .yelow:
             ligthRed.alpha = lightOff
             lightYelow.alpha = lightOn
-        } else if lightYelow.alpha == lightOn {
-            ligthRed.alpha = lightOff
+            conditionLight = .green
+
+        case .green:
             lightYelow.alpha = lightOff
             lightGreen.alpha = lightOn
+            conditionLight = .red
+
         }
     }
-    
 }
 
